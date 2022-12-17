24afd38e1c9f8e37eb5593eea9bd5ec636bb9e62
Verkin78 <otrajenie.jc@gmail.com>
2022-12-17 22:20:12 +0300

    Исправления

--- src/Main.java
+++ src/Main.java
@@ -37,43 +37,6 @@ public class Main {
         System.out.println(numbers);
         MultiplicationTable.countMathTable();
 
-        /*Product banana = new Product("Бфнан", "59 руб.", "1 кг");
-        Product pineapple = new Product("Ананас", "138 руб.", "1 кг");
-        Product apple = new Product("Яблоко", "72 руб.", "1 кг");
-        Product egg = new Product("Яйцо куриное", "68 руб.", "1 кг");
-        Product biff = new Product("Говядина, вырезка", "570 руб.", "1 кг");
-        Product sausage = new Product("Сосиски свиные", "354 руб.", "1 кг");
-        Product cheese = new Product("Сыр", "553 руб.", "1 кг");
-        Product cookies = new Product("Печенье", "187 руб.", "1 кг");
-        Product butter = new Product("Масло сливочное", "400 руб.", "1 кг");
-        Product bread = new Product("Холеб", "118 руб.", "1 кг");
-        Product carrot = new Product("Морковь", "43 руб.", "1 кг");
-        Product potato = new Product("Картофель", "25 руб.", "1 кг");
-        Product candy = new Product("Конфеты шоколадные", "465 руб.", "1 кг");
-        Product chicken = new Product("Курица, бедро", "350 руб.", "1 кг");
-        Product tomato = new Product("Томаты", "160 руб.", "1 кг");
-
-        Set<Product> products = new HashSet<>();
-        products.add(apple);
-        products.add(banana);
-        products.add(biff);
-        products.add(bread);
-        products.add(butter);
-        products.add(candy);
-        products.add(carrot);
-        products.add(cheese);
-        products.add(chicken);
-        products.add(cookies);
-        products.add(egg);
-        products.add(pineapple);
-        products.add(potato);
-        products.add(sausage);
-        products.add(tomato);
-
-        System.out.println(products);
-
-        Set<Recipe> recipes = new HashSet<>();
-        Recipe chicken1 = new Recipe("", "", cheese, cheese, potato, egg, pineapple);
-        System.out.println(chicken1);*/
+      Product.info();
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,6 @@
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Product {
 
@@ -12,13 +14,6 @@ public class Product {
         this.productWeight = productWeight;
     }
 
-    public String theSame() {
-        if (productName == productName) {
-            throw new IllegalArgumentException("Этот товар уже есть в списке");
-        }
-        return
-                this.productName;
-    }
 
     public String getProductName() {
         return productName;
@@ -61,6 +56,9 @@ public class Product {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Product product = (Product) o;
+            if (productName.equals(product.productName)) {
+                throw new IllegalArgumentException("Добавлен одинаковый товар");
+            }
         return Objects.equals(productName, product.productName) && Objects.equals(productPrice,
                 product.productPrice) && Objects.equals(productWeight, product.productWeight);
     }
@@ -79,6 +77,48 @@ public class Product {
                 '}';
     }
 
+    public static void info() {
+
+
+        Product banana = new Product("Бфнан", "59 руб.", "1 кг");
+        Product pineapple = new Product("Ананас", "138 руб.", "1 кг");
+        Product apple = new Product("Яблоко", "72 руб.", "1 кг");
+        Product egg = new Product("Яйцо куриное", "68 руб.", "1 кг");
+        Product biff = new Product("Говядина, вырезка", "570 руб.", "1 кг");
+        Product sausage = new Product("Сосиски свиные", "354 руб.", "1 кг");
+        Product cheese = new Product("Сыр", "553 руб.", "1 кг");
+        Product cookies = new Product("Печенье", "187 руб.", "1 кг");
+        Product butter = new Product("Масло сливочное", "400 руб.", "1 кг");
+        Product bread = new Product("Холеб", "118 руб.", "1 кг");
+        Product carrot = new Product("Морковь", "43 руб.", "1 кг");
+        Product potato = new Product("Картофель", "25 руб.", "1 кг");
+        Product candy = new Product("Конфеты шоколадные", "465 руб.", "1 кг");
+        Product chicken = new Product("Курица, бедро", "350 руб.", "1 кг");
+        Product tomato = new Product("Томаты", "160 руб.", "1 кг");
+
+        Set<Product> products = new HashSet<>();
+        products.add(apple);
+        products.add(banana);
+        products.add(biff);
+        products.add(bread);
+        products.add(butter);
+        products.add(candy);
+        products.add(carrot);
+        products.add(cheese);
+        products.add(chicken);
+        products.add(cookies);
+        products.add(egg);
+        products.add(pineapple);
+        products.add(potato);
+        products.add(sausage);
+        products.add(tomato);
+
+        System.out.println(products);
+
+        Set<Recipe> recipes = new HashSet<>();
+        Recipe chicken1 = new Recipe("", "", cheese, cheese, potato, egg, pineapple);
+        System.out.println(chicken1);
+    }
     }
 
 
